874f456988f18b0dcdfb18b512c5fd36e4122a3c
张华 <15901122405@163.com>
2022-02-23 08:12:40 +0800

    导入登录注册页面

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,9 +8,19 @@ module.exports = {
           component: resolve(__dirname, 'pages/layout'),
           children: [
             {
-              path: '', // 默认子路哟画
+              path: '', // 默认子路由
               name: 'home',
               component: resolve(__dirname, 'pages/home')
+            },
+            {
+              path: '/login', // 登录路由
+              name: 'login',
+              component: resolve(__dirname, 'pages/login')
+            },
+            {
+              path: '/register', // 注册路由
+              name: 'register',
+              component: resolve(__dirname, 'pages/login')
             }
           ]
         }
---
+++ pages/login/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="auth-page">
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-6 offset-md-3 col-xs-12">
+          <h1 class="text-xs-center">{{ isLogin ? "Sing in" : "Sign up" }}</h1>
+          <p class="text-xs-center">
+            <a v-if="isLogin" href="/register">Need an account?</a>
+            <a v-else href="/login">Have an account?</a>
+          </p>
+
+          <ul class="error-messages">
+            <li>That email is already taken</li>
+          </ul>
+
+          <form>
+            <fieldset v-if="!isLogin" class="form-group">
+              <input
+                class="form-control form-control-lg"
+                type="text"
+                placeholder="Your Name"
+              />
+            </fieldset>
+            <fieldset class="form-group">
+              <input
+                class="form-control form-control-lg"
+                type="text"
+                placeholder="Email"
+              />
+            </fieldset>
+            <fieldset class="form-group">
+              <input
+                class="form-control form-control-lg"
+                type="password"
+                placeholder="Password"
+              />
+            </fieldset>
+            <button class="btn btn-lg btn-primary pull-xs-right">
+              {{ isLogin ? "Sing in" : "Sign up" }}
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  computed: {
+    isLogin() {
+      return this.$route.name === "login";
+    }
+  }
+};
+</script>
+
+<style></style>
