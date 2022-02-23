9cdbe380e365944ef6d1b07260220be5d5b0c330
张华 <15901122405@163.com>
2022-02-23 08:44:44 +0800

    处理顶部导航链接

--- pages/layout/index.vue
+++ pages/layout/index.vue
@@ -3,27 +3,35 @@
     <!-- 顶部导航 -->
     <nav class="navbar navbar-light">
       <div class="container">
-        <a class="navbar-brand" href="index.html">conduit</a>
+        <nuxt-link class="navbar-brand" to="/">conduit</nuxt-link>
         <ul class="nav navbar-nav pull-xs-right">
           <li class="nav-item">
             <!-- Add "active" class when you're on that page" -->
-            <a class="nav-link active" href="">Home</a>
+            <nuxt-link class="nav-link active" to="/">Home</nuxt-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="">
+            <nuxt-link class="nav-link" to="/editor">
               <i class="ion-compose"></i>&nbsp;New Article
-            </a>
+            </nuxt-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="">
+            <nuxt-link class="nav-link" to="/settings">
               <i class="ion-gear-a"></i>&nbsp;Settings
-            </a>
+            </nuxt-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="">Sign in</a>
+            <nuxt-link class="nav-link" to="/login">Sign in</nuxt-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="">Sign up</a>
+            <nuxt-link class="nav-link" to="/register">Sign up</nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link class="nav-link" to="/profile/2"
+              ><img
+                src="https://api.realworld.io/images/smiley-cyrus.jpeg"
+                alt=""
+              />zzz</nuxt-link
+            >
           </li>
         </ul>
       </div>
