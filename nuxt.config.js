dffcfc3ccf096d625deab6a358a7091e38cf0d6e
张华 <15901122405@163.com>
2022-02-23 08:34:03 +0800

    导入其他页面

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,12 +15,32 @@ module.exports = {
             {
               path: '/login', // 登录路由
               name: 'login',
-              component: resolve(__dirname, 'pages/login')
+              component: resolve(__dirname, 'pages/login/')
             },
             {
               path: '/register', // 注册路由
               name: 'register',
-              component: resolve(__dirname, 'pages/login')
+              component: resolve(__dirname, 'pages/login/')
+            },
+            {
+              path: '/profile/:username', // 个人中心
+              name: 'profile',
+              component: resolve(__dirname, 'pages/profile/')
+            },
+            {
+              path: '/settings', // 
+              name: 'settings',
+              component: resolve(__dirname, 'pages/settings/')
+            },
+            {
+              path: '/article/:slug', // 
+              name: 'article',
+              component: resolve(__dirname, 'pages/article/')
+            },
+            {
+              path: '/editor', // 注册路由
+              name: 'editor',
+              component: resolve(__dirname, 'pages/editor/')
             }
           ]
         }
