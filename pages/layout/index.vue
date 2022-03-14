a9f7f1c1dffa1c49ec09e0954ee21e0dd3e07411
张华 <15901122405@163.com>
2022-03-14 09:22:54 +0800

    tag更新测试,修改首页面底部信息

--- pages/layout/index.vue
+++ pages/layout/index.vue
@@ -47,10 +47,10 @@
     <footer>
       <div class="container">
         <a href="/" class="logo-font">conduit</a>
+        <span>666</span>
         <span class="attribution">
           An interactive learning project from
-          <a href="https://thinkster.io">Thinkster</a>. Code &amp; design
-          licensed under MIT.
+          <a href="/">Thinkster</a>. Code &amp; design licensed under MIT.
         </span>
       </div>
     </footer>
